2ed5819cc57d4f93c703fe97345a796fbe3bc3c5
novoden <novodenvdk@gmail.com>
2024-12-02 21:04:29 +1000

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,6 @@
+my_set = {'Ivan': 89001231212, 'Egor': 89001231213, 'Urii': 89001231214}
+print (my_set)
+my_set.update({'Pavel': 89001231215, 'Andrey': 89001231216})
+print (my_set)
+del my_set ['Andrey']
+print (my_set)
