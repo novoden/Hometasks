3a8fd02267d4568ab7d74cef8a1f196728e573d6
novoden <novodenvdk@gmail.com>
2024-12-18 18:55:01 +1000

    Create module_2_2.py

---
+++ Module2/module_2_2.py
@@ -0,0 +1,13 @@
+first = input()
+second = input()
+third = input()
+
+if first == second and second == third and first == third:
+    print  ('3')
+        elif first == second:
+            print ('2')
+                elif second == third:
+                    print  ('2')
+                        elif first == third:
+                            print  ('2')
+else: print ('0')
