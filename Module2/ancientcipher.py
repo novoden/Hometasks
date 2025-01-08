7f402148b1edaf005e65b1821c901de74865b20f
novoden <novodenvdk@gmail.com>
2025-01-08 20:09:12 +1000

    Update ancientcipher.py

--- Module2/ancientcipher.py
+++ Module2/ancientcipher.py
@@ -1,1 +1,14 @@
-ancient cipher
+import random
+
+def pasw(k):
+    calc = ''
+    for i in range(1, k):
+        for j in range(i +1, k +1):
+            if k % (i +j) == 0:
+                calc += str(i) + str(j)
+    return f'{k} - {calc}'
+
+initd = range(3, 20)
+k = (random.choice(initd))
+result = pasw(k)
+print(result)
