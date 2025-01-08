722f65a961f4d74496b3cb1dcc5c04aeebf73957
novoden <novodenvdk@gmail.com>
2025-01-08 20:46:23 +1000

    Update module_2_3.py

--- Module2/module_2_3.py
+++ Module2/module_2_3.py
@@ -1,6 +1,12 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
+
 while index < len(my_list):
-    if (my_list[index] % 2 == 0) and (my_list[index] > 0):
-        print(my_list[index])
-    index += 1
+    i = my_list[index]
+    index = index + 1
+    if i == 0:
+        continue
+    elif i < 0:
+        break
+    else:
+        print(i)
