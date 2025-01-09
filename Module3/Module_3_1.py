0438e8aa837a39c5e54ab98ca63ef39989832bcc
novoden <novodenvdk@gmail.com>
2025-01-09 20:35:37 +1000

    Update Module_3_1.py

--- Module3/Module_3_1.py
+++ Module3/Module_3_1.py
@@ -1,1 +1,29 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
 
+def string_info(string):
+    data_string = str(string)
+    result = (len(data_string), data_string.upper(), data_string.lower())
+    count_calls()
+    return result
+
+def is_contains(string, list_to_search):
+    string = str(string).lower()
+    list_to_search = list(list_to_search)
+    count_calls()
+    for i in range(len(list_to_search)):
+        if str(list_to_search[i]).lower() == string:
+            result = True
+            break
+        else:
+            result = False
+            continue
+    return result
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
